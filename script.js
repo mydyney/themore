c01232fc6a0e8796087bf4d51f6483b9115dc0f3
Tommy Go <tommygo@Tommys-Mac-mini.local>
2025-12-13 21:13:59 +0900

    Update fee rate from 0.93% to 1.1%

--- script.js
+++ script.js
@@ -100,9 +100,9 @@ document.addEventListener('DOMContentLoaded', () => {
                             const rawUsd = 100 * parseFloat(usdToJpy);
                             const rawUsdRounded = Number(rawUsd.toFixed(2));
 
-                            // Pure Exchange Rate (No Fees) -> Final Fee 0.93%
+                            // Pure Exchange Rate (No Fees) -> Final Fee 1.1%
                             const baseKrw = Math.floor(rawUsdRounded * usdToKrw);
-                            const finalKrw = Math.floor(baseKrw * 1.0093);
+                            const finalKrw = Math.floor(baseKrw * 1.011);
 
                             rate100 = finalKrw;
                             isRateFinal = true;
